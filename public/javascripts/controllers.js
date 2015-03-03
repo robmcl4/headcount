4630cdf3fdafbe6a55c935ed14117dee33555568
Robert McLaughlin <robert@sparkk.us>
2015-03-02 19:24:09 -0500

    add front-end headcount submit capability

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,4 +1,4 @@
-var headcountApp = angular.module('headcountApp', []);
+var headcountApp = angular.module('headcountApp', ['angularMoment']);
 
 
 headcountApp.controller('recentHeadcount', ['$scope', '$http', function($scope, $http) {
@@ -8,9 +8,32 @@ headcountApp.controller('recentHeadcount', ['$scope', '$http', function($scope, 
     url: '/api/headcount/recent'
   }).success(function(data, status) {
     $scope.headcounts = data;
-    console.log(data);
   }).error(function(data, status) {
     console.error('Error occurred: ' + status);
   });
-  $('#datetimePickerWhen').datetimepicker();
+
 }]);
+
+
+headcountApp.controller('enterHeadcount', function($scope) {
+  var now = moment();
+  now.minutes(Math.round(now.minutes()/30)*30);
+  $scope.when = now;
+
+  $scope.submit = function() {
+    console.log('Submitting!');
+  }
+
+});
+
+headcountApp.directive('datetimePickerRounded', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var model = $(element).find('input').attr('ng-model');
+      var default_ = scope[model];
+      $(element).datetimepicker({defaultDate: default_});
+      scope[model] = $(element).find('input').val();
+    }
+  }
+})
