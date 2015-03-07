54a7dedac7c4a87f0f0d1867f9fd85849df64add
Robert McLaughlin <robert@sparkk.us>
2015-03-07 01:21:10 -0500

    add more sane ordering

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET /api/headcount/recent */
 router.get('/headcount/recent', function(req, res, next) {
   var limit = Math.max(Math.min(parseInt(req.query.limit) || 25, 25), 1);
-  req.models.headcount.find(['ts', 'Z'], limit, function(err, headcounts) {
+  req.models.headcount.find(['-ts', '-id'], limit, function(err, headcounts) {
     if (err)
       next(err);
     else
