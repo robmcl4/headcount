e48f8acf429474df55846598c14ca6d67fb1f6b9
Robert McLaughlin <robert@sparkk.us>
2015-03-20 13:26:48 -0400

    remove timezones!!!

--- db/schema.sql
+++ db/schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE headcount (
 	id       SERIAL PRIMARY KEY,
 	how_many SMALLINT CHECK (how_many >= 0),
 	initials VARCHAR(10) NOT NULL,
-	ts       TIMESTAMP WITH TIME ZONE
+	ts       TIMESTAMP
 );
